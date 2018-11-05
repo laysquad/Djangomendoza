c8e004735df4d25a60e91035d1f40b2afe0c10b8
= <=>
2018-11-05 04:34:54 -0600

    Octavo Commit

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     re_path(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     re_path(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     re_path(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    re_path(r'^accounts/login/$', views.LoginView.as_view(), name='login'),
+    re_path(r'^accounts/logout/$', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
 ]
